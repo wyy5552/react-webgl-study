3b14fd71c8277724f93fb48ff33b6a99c5ec3ebd
wyy5552@163.com <wyy555@qq.com>
2020-07-08 15:59:52 +0800

    'react入門'

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div>
+            你好，世界！
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/FirstPage.js
@@ -0,0 +1,15 @@
+
+import React, { Component } from 'react';
+import './FirstPage.css';
+
+class FirstPage extends Component {
+    render() {
+        return (
+            <div className="first-page">
+               hello,FirstPage!
+            </div>
+        );
+    }
+}
+
+export default FirstPage;
